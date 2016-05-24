82b903b336a087f3afc111eb5ebc806b296fa8e2
vmanamino <vmanamino@gmail.com>
2016-05-24 04:03:10 +0000

    adding month days

--- app/app.js
+++ app/app.js
@@ -1,3 +1,17 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', []).
 
 // your controller and directive code go here
+
+    directive('calendarDisplay', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'table.html',
+            scope: true,
+            transclude: true,
+            controller: function($scope, $element){
+                $scope.calendar = {
+                    dates: CalendarRange.getMonthlyRange(new Date("May 01, 2016 00:00:00"))
+                }
+            }
+        };
+    })
